04da8fd70bfad00e81a12d24eadca5717a504754
DenzelSE <t.d.selokela@gmail.com>
2024-11-12 23:28:41 +0200

    global exception error and configs on app properties

---
+++ src/main/java/com/project/Book_management_system/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.project.Book_management_system.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BookNotFoundException.class)
+    public ResponseEntity<Object> handleBookNotFoundException(BookNotFoundException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", ex.getMessage());
+        
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+}
